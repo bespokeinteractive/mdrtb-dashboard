ff586fb126aee249352e62c5e7376361e36f13dd
Dennys Henry <daugmaud@gmail.com>
2017-03-31 20:06:05 +0300

    Added User-Location Service

--- omod/src/main/java/org/openmrs/module/mdrtbdashboard/fragment/controller/HeaderFragmentController.java
+++ omod/src/main/java/org/openmrs/module/mdrtbdashboard/fragment/controller/HeaderFragmentController.java
@@ -3,16 +3,23 @@ package org.openmrs.module.mdrtbdashboard.fragment.controller;
 import org.openmrs.Patient;
 import org.openmrs.PatientProgram;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.mdrtb.model.UserLocation;
+import org.openmrs.module.mdrtb.service.MdrtbService;
 import org.openmrs.module.mdrtbdashboard.api.MdrtbDashboardService;
 import org.openmrs.module.mdrtbdashboard.model.PatientProgramDetails;
+import org.openmrs.module.mdrtbdashboard.util.LocationModel;
+import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentConfiguration;
 import org.openmrs.ui.framework.fragment.FragmentModel;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Dennis Henry on 1/27/2017.
  */
+
 public class HeaderFragmentController {
     public void controller(FragmentModel model,
                            FragmentConfiguration config,
@@ -27,4 +34,18 @@ public class HeaderFragmentController {
         model.addAttribute("patientDetails", patientDetails);
     }
 
+    public SimpleObject getLoginLocations(UiUtils ui){
+        List<UserLocation> locales = Context.getService(MdrtbService.class).getUserLocations();
+        List<LocationModel> models = new ArrayList<LocationModel>();
+
+        for (UserLocation locale : locales){
+            LocationModel lm = new LocationModel();
+            lm.setId(locale.getLocation().getId());
+            lm.setName(locale.getLocation().getName());
+
+            models.add(lm);
+        }
+
+        return SimpleObject.create("locations", models, "status", "success");
+    }
 }
---
+++ omod/src/main/java/org/openmrs/module/mdrtbdashboard/util/LocationModel.java
@@ -0,0 +1,25 @@
+package org.openmrs.module.mdrtbdashboard.util;
+
+/**
+ * Created by daugm on 3/31/2017.
+ */
+public class LocationModel {
+    Integer id;
+    String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
